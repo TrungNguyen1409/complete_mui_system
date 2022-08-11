bd500f2c594648381ce0a3eb60accbfa381f60ea
Duc Trung Nguyen <96893597+TrungNguyen1409@users.noreply.github.com>
2022-08-11 12:11:12 +0200

    create first components: Heade and SideMenu

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import SideMenu from '../components/SideMenu';
+import Header from '../components/Header';
 
 function App() {
   return (
-    <div>here we go</div>
+    <>
+      <Header></Header>
+      <SideMenu></SideMenu>
+      <div>here we go</div>
+    </>
   );
 }
 
---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SideMenu() {
+  return (
+    <>
+        <div className="side-menu">
+
+        </div>
+
+    </>
+  )
+}
+
+export default SideMenu
